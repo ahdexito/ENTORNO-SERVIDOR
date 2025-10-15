2cac7868e7e3c0685fddc35d96e44164d64e8e40
Ahdexito <ags120120@gmail.com>
2025-10-15 19:49:47 +0200

    save

--- tema02/ejercicios04 funciones recursividad/ejer05.php
+++ tema02/ejercicios04 funciones recursividad/ejer05.php
@@ -5,8 +5,33 @@
     5. Escribe un programa en php que tenga una función para calcular potencias. La función debe recibir como parámetros la base y el exponente, que es opcional y tiene valor por defecto 2 (para elevar al cuadrado).
     */
 
-    function calcular_potencias($base, $exponente) {
+    function calcular_potencias($base, $exponente = 2) {
         
+        $resultado = $base;
+
+        // iterar bucle según el tamaño del exponente
+        for ($i = 1; $i < $exponente; $i++) {
+
+            // acumular la multiplicación cada iteración
+            $resultado *= $base;
+        }
+
+        return $resultado;
     }
+
+    // probar la función pasando el valor del exponente
+    $base = 3;
+    $exponente = 3;
+    $resultado = calcular_potencias($base, $exponente);
+
+    echo "<h3>Función con exponente como parámetro</h3>";
+    echo "<p>El resultado de elevar $base a $exponente es: $resultado</p>";
+
+    // probar la función sin pasar el valor del exponente
+    $base = 5;
+    $resultado = calcular_potencias($base);
+
+    echo "<h3>Función sin exponente como parámetro (2 por defecto)</h3>";
+    echo "<p>El resultado de elevar $base a 2 es: $resultado</p>";
 
 ?>
---
+++ tema02/ejercicios04 funciones recursividad/ejer06.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<h1> EJERCICIO 6 </h1>";
+
+    /*
+    6. Realiza un programa en php que tenga una función recursiva que calcule el factorial de un número, que recibirá como argumento. La función debe devolver el factorial o -1 si el número no es válido.
+    */
+
+    
+?>
