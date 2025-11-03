445ae16ea0a6b7d878a4ce346e470f632696409f
Ahdexito <ags120120@gmail.com>
2025-11-03 11:04:00 +0100

    save

---
+++ tema03/ejercicios01 programacion web/ejer04/result_login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        body {
+            padding: 30px;
+        }
+    </style>
+    <title>resultado</title>
+</head>
+<body>
+    <?php
+        $usuario = [
+            "email" => $_POST["email"],
+            "contra" => $_POST["contra"]
+        ];
+
+        echo "<h1>Registro</h1>";
+
+        $email_sanitizado = filter_var($usuario["email"], FILTER_SANITIZE_EMAIL);
+        $patron_seguro =  '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/';
+
+        if (preg_match($patron_seguro, $usuario["contra"])) {
+            echo "<p>Usuario: " . $usuario["email"] . "</p>";
+            echo "<p>Contraseña: " . $usuario["contra"] . "</p>";
+        }
+        
+        else {
+            echo "<p>La contraseña no cumple con los requisitos de seguridad.</p>";
+        }
+    ?>
+    <a href="ej4_form_login.php"><button class="btn btn-primary">Volver</button></a>
+
+    <footer><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script></footer>
+</body>
+</html>
---
+++ tema03/ejercicios01 programacion web/ejer05/ej5_form_registro.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>formulario</title>
+    <style>
+        body {
+            padding: 30px;
+        }
+        form {
+            background-color: lightsteelblue;
+            padding: 30px;
+            width: 500px;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+<form method="post" action="result_login.php">
+        <h1>Iniciar sesión</h1>
+
+        <label for="nombre">Nombre</label><br>
+        <input type="text" name="nombre" id="nombre" class="form-control-sm" placeholder="Tu nombre">
+
+        <br><br>
+
+        <label for="apellidos">Apellidos</label><br>
+        <input type="text" name="apellidos" id="apellidos" class="form-control-sm" placeholder="Tus apellidos">
+
+        <br><br>
+
+        <label for="email">Email</label><br>
+        <input type="email" name="email" id="email" class="form-control-sm" placeholder="Tu email">
+        
+        <br><br>
+
+        <label for="contra">Contraseña</label><br>
+        <input type="password" name="contra" id="contra" class="form-control-sm" placeholder="Tu contraseña">
+        
+        <br><br>
+        
+        <legend>Selecciona tu género:</legend>
+        <input type="radio" id="masculino" name="genero" value="masculino">
+        <label for="masculino">Masculino</label>
+        <br>
+        <input type="radio" id="feminino" name="genero" value="femenino">
+        <label for="femenino">Femenino</label>
+        <br>
+        <input type="radio" id="otro" name="genero" value="otro">
+        <label for="otro">Otro</label>
+        
+        <br><br>
+
+        <label for="direccion">Dirección</label><br>
+        <input type="text" name="direccion" id="direccion" class="form-control-sm" placeholder="Tu direccion">
+
+        <br><br>
+
+        <label for="postal">Código Postal</label><br>
+        <input type="text" name="postal" id="postal" class="form-control-sm" placeholder="Tu código postal">
+
+        <br><br>
+
+        <label for="poblacion">Población</label><br>
+        <input type="text" name="poblacion" id="poblacion" class="form-control-sm" placeholder="Tu población">
+
+        <br><br>
+
+        <label for="provincia">Provincia</label>
+        <select name="provincia" id="provincia">
+            <option>Selecciona una opción</option>
+            <option value="alava">Álava</option>
+            <option value="albacete">Albacete</option>
+            <option value="alicante">Alicante</option>
+            <option value="almeria">Almería</option>
+            <option value="asturias">Asturias</option>
+            <option value="avila">Ávila</option>
+            <option value="badajoz">Badajoz</option>
+            <option value="barcelona">Barcelona</option>
+            <option value="burgos">Burgos</option>
+            <option value="caceres">Cáceres</option>
+            <option value="cadiz">Cádiz</option>
+            <option value="cantabria">Cantabria</option>
+            <option value="castellón">Castellón</option>
+            <option value="ciudadreal">Ciudad Real</option>
+            <option value="cordoba">Córdoba</option>
+            <option value="cuenca">Cuenca</option>
+            <option value="girona">Girona</option>
+            <option value="granada">Granada</option>
+            <option value="guadalajara">Guadalajara</option>
+            <option value="guipuzcoa">Guipúzcoa</option>
+            <option value="huelva">Huelva</option>
+            <option value="huesca">Huesca</option>
+            <option value="baleares">Islas Baleares</option>
+            <option value="jaen">Jaén</option>
+            <option value="acorunya">A Coruña</option>
+            <option value="larioja">La Rioja</option>
+            <option value="laspalmas">Las Palmas</option>
+            <option value="leon">León</option>
+            <option value="lleida">Lleida</option>
+            <option value="lugo">Lugo</option>
+            <option value="madrid">Madrid</option>
+            <option value="malaga">Málaga</option>
+            <option value="murcia">Murcia</option>
+            <option value="navarra">Navarra</option>
+            <option value="ourense">Ourense</option>
+            <option value="palencia">Palencia</option>
+            <option value="pontevedra">Pontevedra</option>
+            <option value="salamanca">Salamanca</option>
+            <option value="tenerife">Santa Cruz de Tenerife</option>
+            <option value="segovia">Segovia</option>
+            <option value="sevilla">Sevilla</option>
+            <option value="soria">Soria</option>
+            <option value="tarragona">Tarragona</option>
+            <option value="teruel">Teruel</option>
+            <option value="toledo">Toledo</option>
+            <option value="valencia">Valencia</option>
+            <option value="valladolid">Valladolid</option>
+            <option value="vizcaya">Vizcaya</option>
+            <option value="zamora">Zamora</option>
+            <option value="zaragoza">Zaragoza</option>
+        </select>
+
+        <br><br>
+
+        <a href="#" style="color: #000">¿Olvidaste la contraseña?</a>
+        
+        <br><br>
+
+        <button type="submit" class="btn btn-primary">Login</button>
+        
+        <br><br>
+
+        <p>¿Todavía no tienes cuenta? 
+            <a href="#" style="color: red;">Regístrate</a>
+        </p>
+    </form>
+    
+    <footer><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script></footer>
+</body>
+</html>
