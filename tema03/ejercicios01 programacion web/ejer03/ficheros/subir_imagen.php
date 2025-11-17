be2be6f38a20a859e4bfd9497628e148cba32eb7
Ahdexito <ags120120@gmail.com>
2025-11-17 13:26:49 +0100

    save

--- tema03/ejercicios01 programacion web/ejer03/ficheros/subir_imagen.php
+++ tema03/ejercicios01 programacion web/ejer03/ficheros/subir_imagen.php
@@ -7,11 +7,27 @@
 </head>
 <body>
     <?php
-        $nombre_formulario = $_POST["nombre"];
-
-        echo "<h2>$nombre_formulario</h2>";
-
         $directorio = "img/";
+        $nombre_tmp = $_FILES["imagen"]["tmp_name"];
+        $nombre = uniqid('', true) . $_FILES["imagen"]["name"];
+        $ruta_final = $directorio . $nombre;
+
+        $fileName = basename($nombre);
+        $extensionArchivo = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    
+        $extensionPermitida = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg'];
+        if (in_array($extensionArchivo, $extensionPermitida)) {
+            if (move_uploaded_file($nombre_tmp, $ruta_final)) {
+                echo "<img src='$ruta_final'>";
+                echo "<h1>" . $_POST['nombre'] . "</h1>";
+            } else {
+                echo "Error al subir el archivo.";
+                
+            }
+        } else {
+            echo "Extensión de archivo no permitida.";
+            
+        }
     ?>
 </body>
 </html>
